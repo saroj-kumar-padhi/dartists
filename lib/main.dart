b2f4027682f73ba38eceaef9f87da7c2220c49c7
Ritesh Sinha <ritesh.sinha.737001@gmail.com>
2022-04-07 12:36:18 +0530

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
+import 'package:dartists/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+List<String> categories = [
+  "Motivation",
+  "Adventure",
+  "Manga",
+];
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +16,199 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        fontFamily: 'hanuman',
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SplashScreen(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage();
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  TabController? controller;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  var index = 0.obs;
+
+  @override
+  void initState() {
+    controller = TabController(length: 3, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller?.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: Text(
+          "Tiny Coders",
+          style: GoogleFonts.poppins(),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(children: [
+        SingleChildScrollView(
+          child: Obx(
+            () => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "${categories[index.value]}",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontFamily: 'hanuman',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Image From source code",
+                      style: TextStyle(fontFamily: 'hanuman', fontSize: 18),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 200,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+                      itemBuilder: (context, index) => ImageCard(
+                          "assets/images/${categories[controller!.index]}/${index + 1}.jpg"),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Image from internet url",
+                      style: TextStyle(fontFamily: 'hanuman', fontSize: 18),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 200,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+                      itemBuilder: (context, index) => ImageCard(
+                          "assets/images/${categories[controller!.index]}/${index + 1}.jpg"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Spacer(),
+        Positioned(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Container(
+                height: 45,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade700,
+                  borderRadius: BorderRadius.circular(
+                    25.0,
+                  ),
+                ),
+                child: TabBar(
+                    unselectedLabelColor: Colors.black38,
+                    onTap: (tabIndex) {
+                      index.value = tabIndex;
+                    },
+                    indicator: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        25.0,
+                      ),
+                      color: Colors.green,
+                    ),
+                    controller: controller,
+                    tabs: [
+                      Tab(
+                          child: Text(categories[0],
+                              style: TextStyle(
+                                  color: Colors.white60, fontSize: 11))),
+                      Tab(
+                          child: Text(categories[1],
+                              style: TextStyle(
+                                  color: Colors.white60, fontSize: 11))),
+                      Tab(
+                          child: Text(categories[2],
+                              style: TextStyle(
+                                  color: Colors.white60, fontSize: 11))),
+                      // Tab(
+                      //     child: Text(categories[3],
+                      //         style: TextStyle(
+                      //             color: Colors.white60, fontSize: 11))),
+                    ]),
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class ImageCard extends StatelessWidget {
+  String path;
+  ImageCard(this.path);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: AspectRatio(
+        aspectRatio: 0.7,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(image: AssetImage(path), fit: BoxFit.fill),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/splash.dart
@@ -0,0 +1,45 @@
+import 'package:dartists/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  void onReady() {
+    moveToNext();
+  }
+
+  Future<void> moveToNext() async {
+    await Future.delayed(Duration(seconds: 3), () {});
+    Get.to(MyHomePage());
+  }
+
+  Widget build(BuildContext context) {
+    onReady();
+    return Scaffold(
+      backgroundColor: Colors.redAccent[200],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 15,
+            ),
+            Image.asset(
+              "assets/images/Adventure/1.jpg",
+              height: 300,
+              width: 300,
+            ),
+            CircularProgressIndicator(
+              backgroundColor: Colors.deepPurple,
+              valueColor: AlwaysStoppedAnimation(Colors.white),
+              strokeWidth: 5,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
